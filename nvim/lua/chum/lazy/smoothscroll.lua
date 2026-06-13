1918230df5a69086322434232408c81ba0f9e506
tuan-be <tuan.quang.tran@aioz.io>
2026-06-13 10:49:04 +0700

    feat: add neoscroll

---
+++ nvim/lua/chum/lazy/smoothscroll.lua
@@ -0,0 +1,10 @@
+return {
+	"karb94/neoscroll.nvim",
+	opts = {
+		mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
+		hide_cursor = false,
+		stop_eof = true,
+		easing = "quadratic",
+		duration_multiplier = 0.6,
+	},
+}
