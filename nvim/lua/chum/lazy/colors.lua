3b64dfad91d1c10a67f8ab4d12b38c8ec587e5ce
tuan-be <tuan.quang.tran@aioz.io>
2026-06-29 18:15:37 +0700

    feat: change default theme

--- nvim/lua/chum/lazy/colors.lua
+++ nvim/lua/chum/lazy/colors.lua
@@ -3,15 +3,10 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		config = function()
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
 			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
-	},
-	{
-		"mcauley-penney/techbase.nvim",
-		config = function(_, opts)
-			vim.cmd.colorscheme("techbase")
-		end,
-		priority = 1000,
+		opts = {},
 	},
 }
